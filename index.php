471a53bff1189c545cdfafcbccd2570c3baceb8b
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-02 00:51:10 +0600

    done

--- index.php
+++ index.php
@@ -22,16 +22,22 @@
 		?>
 
 	<?php
-	$color = "red";
-	echo "My car color is " . $color. "<br>";
-	$language = "php, html, css, js, node, mongoDB, react.";
-	echo "I am expert in " . $language. "<br>";
+		$color = "red";
+		echo "My car color is " . $color. "<br>";
+		$language = "php, html, css, js, node, mongoDB, react.";
+		echo "I am expert in " . $language. "<br>";
 	?>
 
 	<?php 
 		#This is php variable example.
+		$tex = "this is tex variable.";
 		$myBalance = 5 +10 +10;
 		echo "my balance is total " .$myBalance . "<br>";
+		echo "Hey ". $tex ."<br>"; 
+		# the sum of two variables
+		$x = 5;
+		$y = 4;
+		echo $x + $y;
 	 ?>
 
 </body>

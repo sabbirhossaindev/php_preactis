d2d0b5997748f774d389447a627432d8586940c2
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-02 00:11:41 +0600

    php_preactis

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h4>PHP</h4>
+	<hr>
+	<?php
+		echo "welcome to my php code.";
+	?>
+	<br>
+
+	<?php
+		// This is a php Syntax.
+
+		// comment using
+		# This is also a single-line comment
+		/*
+		This is a multiple-lines comment block
+		that spans over multiple
+		lines
+		*/
+		/* + 15 */
+		?>
+
+	<?php
+	$color = "red";
+	echo "My car color is " . $color. "<br>";
+	$language = "php, html, css, js, node, mongoDB, react.";
+	echo "I am expert in " . $language. "<br>";
+	?>
+
+	<?php 
+		#This is php variable example.
+		$myBalance = 5 +10 +10;
+		echo "my balance is total " .$myBalance . "<br>";
+	 ?>
+
+</body>
+</html>

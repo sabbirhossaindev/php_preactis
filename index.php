71812208ec925985b9c4bb3f5e6120147d4fd56c
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-03 10:22:48 +0600

    done

--- index.php
+++ index.php
@@ -55,5 +55,24 @@
 	 echo('Learn PHP <br>');
 	 print('Learn PHP');
 	 ?>
+
+	 <?php 
+	 #PHP Data Types
+	 /*
+	 Variables can store data of different types, and different data types can do different things.
+	PHP supports the following data types:
+
+	1.String
+	2.Integer
+	3.Float (floating point numbers - also called double)
+	4.Boolean
+	5.Array
+	6.Object
+	7.NULL
+	8.Resource
+	*/
+	#PHP String Example
+	
+	  ?>
 </body>
 </html>

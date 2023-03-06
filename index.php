d711b1e955e66b3b5028b2ce04ca770a1bfab05d
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-07 04:28:04 +0600

    switch case for php

--- index.php
+++ index.php
@@ -336,7 +336,25 @@
 		}else{
 			echo "Your password is wrong";
 		}
-		
+	 ?>
+
+	 <?php 
+	 echo('<h3>PHP switch Statement</h3> <hr>');
+	 $favcolor = "green";
+		switch ($favcolor) {
+		  case "red":
+		    echo "Your favorite color is red!";
+		    break;
+		  case "blue":
+		    echo "Your favorite color is blue!";
+		    break;
+		  case "green":
+		    echo "Your favorite color is green!";
+		    break;
+		  default:
+		    echo "Your favorite color is neither red, blue, nor green!";
+		}
+
 	 ?>
 </body>
 </html>

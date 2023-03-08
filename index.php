716b8141e5fc313395a5856de41535c265e7ae18
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-09 00:58:59 +0600

    function of a php code

--- index.php
+++ index.php
@@ -509,6 +509,36 @@
 		?>
 		*/
 
+		echo "<br>";
+		echo('<p>PHP Default Argument Value Example <br>
+			_______________________</p>');
+		
+		function setHeight(int $minheight = 50) {
+		  echo "The height is : $minheight <br>";
+		}
+
+		setHeight(350);
+		setHeight(); // will use the default value of 50
+		setHeight(135);
+		setHeight(80);
+
+
+		echo "<br>";
+		echo('<p>PHP Functions - Returning values Example <br>
+			_______________________</p>');
+
+		function sum(int $x, int $y) {
+		  $z = $x + $y;
+		  return $z;
+		}
+
+		echo "5 + 10 = " . sum(5, 10) . "<br>";
+		echo "7 + 13 = " . sum(7, 13) . "<br>";
+		echo "2 + 4 = " . sum(2, 4);
+
+		echo "<br>";
+		echo('<p>PHP Return Type Declarations Example <br>
+			_______________________</p>');
 	 ?>
 
 

c40796d953c11938036ebe3ddc859f9ac814e573
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-10 22:59:09 +0600

    array of a php code

--- index.php
+++ index.php
@@ -586,6 +586,19 @@
 		echo "<br>";
 		echo('<p>PHP Multidimensional Arrays<br>
 			_______________________</p>');
+		echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+		echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+		echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+		echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+
+		for ($row = 0; $row < 4; $row++) {
+		  echo "<p><b>Row number $row</b></p>";
+		  echo "<ul>";
+		  for ($col = 0; $col < 3; $col++) {
+		    echo "<li>".$cars[$row][$col]."</li>";
+		  }
+		  echo "</ul>";
+		}
 	?>
 
 

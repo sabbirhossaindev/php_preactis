5fea37b6069df6b032ad3f96b0da442afebde502
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-18 11:57:29 +0600

    darunit of a php code done

---
+++ darun.php
@@ -0,0 +1,14 @@
+<!--  darunit problem one solved. -->
+<?php
+    $numberOne = 5;
+    $numberTwo = 10;
+    $numberThree = 15;
+
+    If($numberOne > $numberTwo){
+        echo 'Number One is a Larger number';
+    }if ($numberTwo > $numberThree) {
+        echo 'Number Two is a Larger number';
+    } else {
+        echo 'Number Three is a Larger number';
+    }
+?>

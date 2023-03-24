95a41182853a2328245a814b169d618b2d7bdf49
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-24 17:28:18 +0600

    form done of php code done

---
+++ student_id_cart.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Id Cart</title>
+</head>
+<body>
+    <!-- name field -->
+    <div>
+        <label for="field" class="label--requered">Name</label>
+        <section>
+            <input id="field" type="text" required placeholder="Sabbir Hossain">
+        </section>
+    </div>
+
+    <!-- phone field -->
+    <div>
+        <label for="field" class="label--requered">Phone</label>
+        <section>
+            <input id="field" type="text" required placeholder="Enter your Number">
+        </section>
+    </div>
+    <!-- email field -->
+    <div>
+        <label for="field" class="label--requered">Email</label>
+        <section>
+            <input id="field" type="email" required placeholder="example@gmail.com">
+        </section>
+    </div>
+    
+</body>
+</html>

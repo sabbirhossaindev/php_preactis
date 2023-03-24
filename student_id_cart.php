a1844467b1e320fa23ef1fc5abec49c926a1cc63
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-24 23:06:07 +0600

    form done of php code done

--- student_id_cart.php
+++ student_id_cart.php
@@ -5,30 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Id Cart</title>
+    <link rel="stylesheet" type="text/css" href="id.css">
 </head>
 <body>
-    <!-- name field -->
-    <div>
-        <label for="field" class="label--requered">Name</label>
-        <section>
-            <input id="field" type="text" required placeholder="Sabbir Hossain">
-        </section>
-    </div>
-
-    <!-- phone field -->
-    <div>
-        <label for="field" class="label--requered">Phone</label>
-        <section>
-            <input id="field" type="text" required placeholder="Enter your Number">
-        </section>
-    </div>
-    <!-- email field -->
-    <div>
-        <label for="field" class="label--requered">Email</label>
-        <section>
-            <input id="field" type="email" required placeholder="example@gmail.com">
-        </section>
-    </div>
+    <section class="student-id-section">
+        <h3 class="discription">Create Your Virtual ID Card</h3>
+        <form action="">
+            Name*   <input type="text" name="name" required placeholder="Name"><br> <br>
+            E-mail*  <input type="text" name="email" required placeholder="email@gmail.com"><br> <br>
+            Phone*   <input type="text" name="phone" required placeholder="Phone"><br> <br>
+            Course*  <input type="text" name="course" required placeholder="Course"><br> <br> 
+            Batch No*  <input type="text" name="batch" required placeholder="Batch"><br> <br>
+            <!-- submit btn -->
+            <div class="btn-div">
+                <button class="btn" type="submit">Genarate ID</button>
+            </div>
+        </form>
+    </section>
     
 </body>
 </html>

57660830e2e5f3251e9c778989f1c30d199469f6
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-26 22:10:52 +0600

    student id card done of php code done

---
+++ sid.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Identity Card |PHP</title>
+    <link rel="stylesheet" href="sid.css">
+</head>
+<body>
+    <section>
+        <div class="sid">
+            <h2 class="sid-h2">FromIT SID</h2>
+        </div>
+    </section>
+</body>
+</html>

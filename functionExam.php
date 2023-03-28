022e087fb7bd8a1130671fc4db2ac0746bc524ce
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-28 22:12:19 +0600

    student id card done of php code done

---
+++ functionExam.php
@@ -0,0 +1,7 @@
+<?php
+    // functionExam
+    function addNumbers($name, $age, $class, $roll, $relegone){
+        echo "I am $name. I am $age years old. I reade in diploma in $class semester in computer subject. My board roll is $roll. I have a $relegone. Alhamdullah.";
+    }
+    addNumbers('sabbir', 20, '7th', '477470', 'Islam' );
+?>

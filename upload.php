85f96155c79c37a7293205539a014adf636b16b9
sabbir hossain <mdsabbir477470@gmail.com>
2023-03-29 16:30:30 +0600

    student id card done of php code done

--- upload.php
+++ upload.php
@@ -1,12 +1,23 @@
+<?php
+    if(isset($_POST['submit'])){
+        $img_name = $_FILES['upload_img']['name'];
+        $img_name = $_FILES['upload_img']['tmp_name'];
+        move_uploaded_file($tmp_name, "upload/".$img_name);
+    }
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Display Image</title>
 </head>
 <body>
-    
+    <img src="img/<?php if(isset($img_name)){echo $img_name;} ?>" alt="$img_name">
 </body>
 </html>
